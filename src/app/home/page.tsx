d0a4c17678100d2640d8de7edf7f962b93f418d1
Kartik Upadhyay <kartiku904@gmail.com>
2024-06-13 15:21:22 +0530

    Added logout functionality in Home section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 
 export default function Home(){
+
+  const router = useRouter();
+
+  const [data, setData] = useState("");
+
+  const logout = async () => {
+    try {
+      
+      await axios.get('/api/users/logout')
+      toast.success("Logout Successful")
+
+      router.push('/')
+
+    } catch (error:any) {
+      console.log(error.message);
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>LANDING PAGE</div>
+    <div className='bg-black flex flex-col items-center justify-center min-h-screen py-2 text-white'>
+      <h1>LANDING PAGE</h1>
+      <hr />
+      <button 
+        onClick={logout}
+        className="text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 hover:text-red-500 hover:bg-white font-bold">
+          LOGOUT
+      </button>
+    </div>
   )
 }
